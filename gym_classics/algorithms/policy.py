b3e20cd2b21880204b1f5e33b98223745e0ca85f
mhahsler <michael@hahsler.net>
2026-01-12 10:17:34 -0600

    added policy iteration.

--- gym_classics/algorithms/policy.py
+++ gym_classics/algorithms/policy.py
@@ -1,11 +1,18 @@
 import numpy as np
-from gym_classics.algorithms.DP import backup
 
 def random_policy(env):
     return np.random.choice(env.actions(), size=len(env.states()))
 
 def encode_policy(env, policy, type = "text"):
     return [env.encode_action(a, type = type) for a in policy]
+
+
+# this is a copy from DP to prevent circular imports.
+def _backup(env, discount, V, state, action):
+    next_states, rewards, terminals, probs = env.model(state, action)
+    bootstraps = (1.0 - terminals) * V[next_states]
+    return np.sum(probs * (rewards + discount * bootstraps))
+
 
 # Note: argmax could be randomized
 def greedy_policy(env, V, discount=1):
@@ -19,7 +26,7 @@ def greedy_policy(env, V, discount=1):
     policy = np.zeros(len(env.states()))
 
     for s in env.states():
-        Q_values = [backup(env, discount, V, s, a) for a in env.actions()]
+        Q_values = [_backup(env, discount, V, s, a) for a in env.actions()]
         policy[s] = np.argmax(Q_values)
 
     return policy
