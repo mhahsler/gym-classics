02bde18317b7dfd803c00b6b4eeb2165e864f8bc
mhahsler <michael@hahsler.net>
2026-04-14 16:34:33 -0500

    fixed history.

--- gym_classics/algorithms/temporal_difference_learning.py
+++ gym_classics/algorithms/temporal_difference_learning.py
@@ -3,12 +3,17 @@ import numpy as np
 from gym_classics.algorithms.policy import random_policy, random_argmax
 from tqdm import tqdm
 
-def Sarsa(env, discount, alpha, epsilon, Q=None, n = 100, verbose = False, returns = False):
-    if returns:
-        Rs = [0] * n 
+def Sarsa(env, discount, alpha, epsilon, Q=None, n = 100, verbose = False, history = False):
         
     if Q is None:
         Q = np.zeros((env.observation_space.n, env.action_space.n))
+    
+    if history:
+        Q_list = []
+        Q_list.append(Q.copy())
+        return_list = []
+        ep_len_list = []
+        
     
     for i in tqdm(range(n), desc="Sarsa", disable=verbose):
         s, r = env.reset()
@@ -23,9 +28,10 @@ def Sarsa(env, discount, alpha, epsilon, Q=None, n = 100, verbose = False, retur
           
         t = 0
         done = False
+        G = 0
         while not done:            
             sp, r, done, _, _ = env.step(a)
-            Rs[i] += r * pow(discount, t)
+            G += r * pow(discount, t)
             t += 1
             
         
@@ -39,25 +45,38 @@ def Sarsa(env, discount, alpha, epsilon, Q=None, n = 100, verbose = False, retur
             if verbose:
                 print(f"{t} - sarsa: {s},{a},{r},{sp},{ap}, - new Q(s,a): {Q[s,a]}")
                 if done:
-                    print("Total return:", Rs[i])
+                    print("Total return:", G)
             
             s = sp
             a = ap
-          
-    if returns:
-        return Q, Rs
+        
+        if history:
+            Q_list.append(Q.copy())
+            return_list.append(G)   
+            ep_len_list.append(t)
+    
+    if history:
+        return Q, {'Qs': Q_list, 'returns': return_list, 'ep_lens': ep_len_list}
           
     return Q
 
 
-def Q_learning(env, discount, alpha, epsilon, Q=None, n = 100, verbose = False):
+def Q_learning(env, discount, alpha, epsilon, Q=None, n = 100, verbose = False, history = False):
     if Q is None:
         Q = np.zeros((env.observation_space.n, env.action_space.n))
     
+    if history:
+        Q_list = []
+        Q_list.append(Q.copy())
+        return_list = []
+        ep_len_list = []
+
     for i in tqdm(range(n), desc="Q-Learning", disable=verbose):
         s, r = env.reset()
           
         done = False
+        G = 0
+        t = 0   
         while not done:
             # epsilon-greedy choice w.r.t. Q 
             if (random.random() > epsilon):
@@ -67,8 +86,20 @@ def Q_learning(env, discount, alpha, epsilon, Q=None, n = 100, verbose = False):
             
             sp, r, done, _, _ = env.step(a)
         
+            if history:
+                G += r * pow(discount, t)
+                t += 1
+        
             Q[s,a] = Q[s,a] + alpha * (r + discount * np.max(Q[sp,:]) - Q[s,a])
             
             s = sp
+        
+        if history:
+            Q_list.append(Q.copy())
+            return_list.append(G)
+            ep_len_list.append(t)
+    
+    if history:
+        return Q, {'Qs': Q_list, 'returns': return_list, 'ep_lens': ep_len_list}
           
     return Q
